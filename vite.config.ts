642100743f80f23b7f2188ecdd79e113834882c4
Arman Omerovic <arman.omerovic@datadesign.me>
2026-03-13 01:20:32 +0100

    Vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {nitro} from "nitro/vite";
+
 
 const config = defineConfig({
   plugins: [
@@ -13,6 +15,7 @@ const config = defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
